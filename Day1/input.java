29899c90995e80cafce1e9b63611193aebb3bfd4
Kundan-ojha <aryagmast111@gmail.com>
2022-03-04 00:17:57 +0530

    site ready to deploy

---
+++ Day1/input.java
@@ -0,0 +1,11 @@
+
+import java.util.*;
+
+public class input {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println(scn.nextInt());
+    }
+}
+    
